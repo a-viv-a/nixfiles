78604d7391332eded6bdae0cfec32fdd2aae1ed8
Aviva Ruben <aviva@rubenfamily.com>
2024-09-19 13:11:49 -0500

    add wallpaper to lock screen

--- wm.nix
+++ wm.nix
@@ -1,4 +1,5 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   programs.river.enable = true;
   environment.systemPackages = with pkgs; [
     way-displays
@@ -22,18 +23,25 @@
   environment.etc."wob/wob.ini".source = ./wob/wob.ini;
 
   environment.etc."swayidle/config".source = ./lock/swayidle;
-  environment.etc."swaylock/config".source = ./lock/swaylock;
+  environment.etc."swaylock/config".source = pkgs.substitute {
+    src = ./lock/swaylock;
+    substitutions = [
+      "--replace-fail"
+      "{{wallpaper}}"
+      (toString ./lock/wallpaper.jpeg)
+    ];
+  };
 
-  security.pam.services.swaylock = {};
+  security.pam.services.swaylock = { };
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.gdm-password.enableGnomeKeyring = true;
-  
+
   # networking.networkmanager.dispatcherScripts = [
   # 		{
   # 			source = pkgs.writeText "updateStatusOnChange" ''
-		# 		#!/usr/bin/env ${pkgs.fish}/bin/fish
-		# 		refresh_status wifi
+  # 		#!/usr/bin/env ${pkgs.fish}/bin/fish
+  # 		refresh_status wifi
   # 			'';
   # 			type = "basic";
   # 		}	
@@ -41,6 +49,11 @@
 
   # https://nixos.wiki/wiki/Sway#Inferior_performance_compared_to_other_distributions
   security.pam.loginLimits = [
-    { domain = "@users"; item = "rtprio"; type = "-"; value = 1; }
+    {
+      domain = "@users";
+      item = "rtprio";
+      type = "-";
+      value = 1;
+    }
   ];
 }
