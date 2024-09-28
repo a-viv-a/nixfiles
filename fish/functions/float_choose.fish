595598103524ed2625caebc371b4a5d306729f41
Aviva Ruben <aviva@rubenfamily.com>
2024-09-28 15:45:56 -0500

    bigger window

--- fish/functions/float_choose.fish
+++ fish/functions/float_choose.fish
@@ -1,6 +1,6 @@
 function float_choose
     set tmp (mktemp -t float_choose.XXXXXXXX)
-    foot -a floating_foot yazi --chooser-file "$tmp"
+    foot -W 140x45 -a floating_foot yazi --chooser-file "$tmp"
     cat -- "$tmp"
     # true if file size greater than zero
     test -s "$tmp"
