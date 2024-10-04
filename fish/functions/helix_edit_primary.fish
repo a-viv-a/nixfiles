9e01a931ff447ba390431db52a1986088e692517
Aviva Ruben <aviva@rubenfamily.com>
2024-10-03 21:04:41 -0500

    bail on helix edit when ctrl c or empty

--- fish/functions/helix_edit_primary.fish
+++ fish/functions/helix_edit_primary.fish
@@ -1,6 +1,11 @@
 function helix_edit_primary
     set template 'helix_edit_primary.XXXXXXXX'
-    set tmp (mktemp -p ~/scratch -t $template --suffix=(read -c '.' -P $template))
+    set suffix (read -c '.' -P $template)
+    if test $status -ne 0 -o -z "$suffix" -o "$suffix" = "."
+        return 1
+    end
+    echo "$suffix"
+    set tmp (mktemp -p ~/scratch -t $template --suffix=$suffix)
     set initial (wl-paste --primary | tee $tmp)
     hx $tmp
     if test "$initial" != "$(cat $tmp)"
