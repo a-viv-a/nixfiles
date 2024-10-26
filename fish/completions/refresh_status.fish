be09ffb155679925276df5f9114203945bfe11d3
Aviva Ruben <aviva@rubenfamily.com>
2024-10-25 23:41:04 -0500

    fix completion typo

--- fish/completions/refresh_status.fish
+++ fish/completions/refresh_status.fish
@@ -1,3 +1,3 @@
 complete -c refresh_status -f
-set status_items (bat .config/river/status | rg '^function (\w+)_fn' -r '$1')
+set status_items (bat ~/.config/river/status | rg '^function (\w+)_fn' -r '$1')
 complete -c refresh_status -a "$status_items"
