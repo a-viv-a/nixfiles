8d069980315b50ca2c36e30ce2bcd0e332e336d0
Aviva Ruben <aviva@rubenfamily.com>
2024-11-23 16:30:36 -0600

    prototype subprocesses that can mutate status

--- fish/completions/refresh_status.fish
+++ fish/completions/refresh_status.fish
@@ -1,3 +1,3 @@
 complete -c refresh_status -f
 set status_items (bat ~/.config/river/status | rg '^function (\w+)_fn' -r '$1')
-complete -c refresh_status -a "$status_items"
+complete -c refresh_status -n "not __fish_seen_subcommand_from $status_items" -a "$status_items"
--- fish/functions/refresh_status.fish
+++ fish/functions/refresh_status.fish
@@ -1,10 +1,10 @@
-function refresh_status
+function refresh_status --argument-names status_item value \
+    -d 'refresh a status bar item by name. value may optionally be provided, otherwise status_item_fn will be run'
     set status_pid_dir "$XDG_RUNTIME_DIR/status_pid"
     set needs_refresh_dir "$XDG_RUNTIME_DIR/status_needs_refresh"
-    for status_item in $argv
-        # mark this status item as needing immediate refresh
-        touch $needs_refresh_dir/$status_item
-    end
+    # mark this status item as needing immediate refresh
+    # if only one argument is provided this results in an empty file, triggering rerun
+    echo $value >$needs_refresh_dir/$status_item
     # wake up status process early
     # this will set awoken to true
     # which will cause the refresh dir to be iterated and acted on
