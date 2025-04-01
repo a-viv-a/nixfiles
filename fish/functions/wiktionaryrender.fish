e8feae010089c06bb9515ce91d0571f5bed78883
Aviva Ruben <aviva@rubenfamily.com>
2025-04-01 12:34:43 -0500

    better wiki renderer

--- fish/functions/wiktionaryrender.fish
+++ fish/functions/wiktionaryrender.fish
@@ -1,20 +1,49 @@
 function wiktionaryrender
     read -l -z str
 
-    set templates \
-        "a|accent" (set_color red) \
-        "s|sense" (set_color blue) \
-        "lb|label" (set_color blue) \
-        "ng|non-gloss" (set_color green)
-
-    set reset_color (set_color normal)
-    for i in (seq 1 2 (count $templates))
-        set pfrag $templates[$i]
-        set pattern "{{(?:$pfrag)\\|((?:.|\\n)*?)}}"
-        set color $templates[(math "$i + 1")]
-        set str (string replace -r -a $pattern "$color\$1$reset_color" $str | string collect)
-        echo $pattern
+    function label
+        set tags $argv[2..]
+        string join ', ' $tags
     end
+
+    set templates \
+        style "a|accent" (set_color red) \
+        style "s|sense" (set_color blue) \
+        vargs "lb|lbl|label" label (set_color blue) \
+        style "ng|non-gloss" (set_color green)
+
+    set reset_style (set_color normal)
+    set --global i 1
+    function i++
+        echo $i
+        set i (math "$i + 1")
+    end
+    while test $i -lt (count $templates)
+        set type $templates[(i++)]
+
+        set pfrag $templates[(i++)]
+        set pattern "{{(?:$pfrag)\\|((?:.|\\n)*?)}}"
+
+        if test "$type" = vargs
+            set fn $templates[(i++)]
+            set style $templates[(i++)]
+            while true
+                set rtarget "{{VARGS_REPLACE_TARGET}}"
+                set scratch (string replace -f -r $pattern $rtarget $str)
+                # no match, continue
+                if test "$status" -ne 0
+                    break
+                end
+                set args (string match -g -r $pattern $str | string split "|")
+                set rinner ($fn $args)
+                set str (string replace $rtarget "$style$rinner$reset_style" $scratch | string collect)
+            end
+        else
+            set style $templates[(i++)]
+            set str (string replace -r -a $pattern "$style\$1$reset_style" $str | string collect)
+        end
+    end
+    set --erase i
 
     echo $str
 end
