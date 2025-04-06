13a74bf18e317a3a2d80987d586c42495dfc0cb3
Aviva Ruben <aviva@rubenfamily.com>
2025-04-06 17:11:50 -0500

    increase tmpfs size to prevent builds from failing lol

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -18,7 +18,7 @@
     {
       device = "none";
       fsType = "tmpfs";
-      options = [ "defaults" "size=25%" "mode=755" ];
+      options = [ "defaults" "size=50%" "mode=755" ];
     };
 
   fileSystems."/boot" =
